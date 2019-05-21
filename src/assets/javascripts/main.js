1cb7c2c3d33dc8aa886ba0d127aed005cfc33eb0
Manindra Gautam <manindragautamofficial@gmail.com>
2019-05-22 02:06:37 +0530

    configured: fontawesome icons library

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import store from 'clientConfig/store';
 import Routes from 'clientConfig/routes';
 
+import 'clientConfig/icons'; // FontAwesome Icons Library
+
 ReactDOM.render(
   <Provider store={store}>
     <Routes />
